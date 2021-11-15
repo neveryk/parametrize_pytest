4dceb6cca36dd3dd670cb223ec174738ce3c33a5
DESKTOP-QJ5T1HA\Administrator <376685258@qq.com>
2021-11-15 16:44:57 +0800

    11-15

---
+++ core/client.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import json
+import allure
+BASE_PATH=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+class Client:
+    def __init__(self,host,**kwargs):
+        self.host=host
+        self.session=requests.session()
+
+    def post(self,url,jsondata=None,**kwargs):
+        
